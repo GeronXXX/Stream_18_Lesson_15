1aa0fd92f05b4086e1513d05010415d6c96b4870
GeronXXX <lityagin.ru@gmail.com>
2023-05-08 20:01:38 +0300

    v.3.4

--- src/test/java/stream18/landing_page_test/landing_page_test/LandingPageTest.java
+++ src/test/java/stream18/landing_page_test/landing_page_test/LandingPageTest.java
@@ -7,19 +7,19 @@ import stream18.landing_page_test.landing_page_test.user_data.TestBase;
 
 @Tag("1ofd_tests")
 public class LandingPageTest extends TestBase {
-    PageObjects pageObgects = new PageObjects();
+    PageObjects pageObjects = new PageObjects();
 
     @Test
     @DisplayName("Проверка, что страница открылась")
     void openPageTest() {
-        pageObgects.openPage()
+        pageObjects.openPage()
                 .textLending("Первый ОФД – первый ", "оператор фискальных данных");
     }
 
     @Test
     @DisplayName("Проверка, что кнопка не задизайблена 'Отправить заявку'")
     void analyticsMenuTest() {
-        pageObgects.openPage()
+        pageObjects.openPage()
                 .menuAnalytics()
                 .salesMonitoringButton()
                 .setStellungnahmeName(FakerTestData.firstName)
@@ -30,7 +30,7 @@ public class LandingPageTest extends TestBase {
     @Test
     @DisplayName("Проверка, поиска чека по ФН, ФД, ФПД")
     void consumerCheckSearchTest() {
-        pageObgects.openPage()
+        pageObjects.openPage()
                 .footerMenuVerificationChek()
                 .numberFn("9289440300712966")
                 .numberFd("186968")
@@ -41,7 +41,7 @@ public class LandingPageTest extends TestBase {
     @Test
     @DisplayName("Проверка, поиска чека по ссылке")
     void consumerCheckSearchLinkSearchTest() {
-        pageObgects.openPage()
+        pageObjects.openPage()
                 .footerMenuVerificationChek()
                 .consumerMenuButtonLinkSearch()
                 .fieldlinkCheck("https://consumer.1-ofd.ru/ticket?t=20210822T1058&s=1600.00&fn" +
@@ -53,7 +53,7 @@ public class LandingPageTest extends TestBase {
     @Test
     @DisplayName("Проверка тултипа на странице регистрации")
     void registrationTooltipTest() {
-        pageObgects.openPage()
+        pageObjects.openPage()
                 .blockEnterButtonRegister()
                 .registrationButtonContinue()
                 .registrationFieldName()
