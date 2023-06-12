7678328a6209ebac68fbf3bd8341b617e5a20762
GeronXXX <lityagin.ru@gmail.com>
2023-06-12 14:18:15 +0300

    v.6

--- src/test/java/stream18/landing_page_test/tests/LandingPageTest.java
+++ src/test/java/stream18/landing_page_test/tests/LandingPageTest.java
@@ -9,9 +9,6 @@ import stream18.landing_page_test.pages.PageHome;
 public class LandingPageTest extends TestBase {
     PageHome pageHome = new PageHome();
     PageAnalitika pageAnalitika = new PageAnalitika();
-//    ObjectCheckSearch objectCheckSearch = new ObjectCheckSearch();
-//    ObjecRegistration objecRegistration = new ObjecRegistration();
-
 
     @Test
     @DisplayName("Проверка, что страница открылась")
@@ -31,35 +28,4 @@ public class LandingPageTest extends TestBase {
                 .setStellungnahmePhone(data.phoneNumber)
                 .verificationlungnahmeButtonNotDisabled();
     }
-//    @Test
-//    @DisplayName("Проверка, поиска чека по ФН, ФД, ФПД")
-//    void consumerCheckSearchTest() {
-//        pageHome.openPage()
-//                .footerMenuVerificationChek();
-//        objectCheckSearch.numberFn("9289440300712966")
-//                .numberFd("186968")
-//                .numberFp("2494402381")
-//                .searchFormButtonFind()
-//                .textCheckFound("Чек найден");
-//    }
-//    @Test
-//    @DisplayName("Проверка, поиска чека по ссылке")
-//    void consumerCheckSearchLinkSearchTest() {
-//        pageHome.openPage()
-//                .footerMenuVerificationChek();
-//        objectCheckSearch.consumerMenuButtonLinkSearch()
-//                .fieldlinkCheck("https://consumer.1-ofd.ru/ticket?t=20210822T1058&s=1600.00&fn" +
-//                        "=9289440300712966&i=186968&fp=2494402381&n=1")
-//                .searchByLinkFormButtonFind()
-//                .textCheckFound("Чек найден");
-//    }
-//    @Test
-//    @DisplayName("Проверка тултипа на странице регистрации")
-//    void registrationTooltipTest() {
-//        pageHome.openPage()
-//                .blockEnterButtonRegister();
-//        objecRegistration.registrationButtonContinue()
-//                .registrationFieldName()
-//                .registrationTooltip("Обязательное поле!");
-//    }
 }
