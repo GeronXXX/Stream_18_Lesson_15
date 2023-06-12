b9cdf20583117343a1e33d55d8440bddc3e3a412
GeronXXX <lityagin.ru@gmail.com>
2023-06-12 11:42:58 +0300

    v.4.5

--- src/test/java/stream18/landing_page_test/tests/TestBase.java
+++ src/test/java/stream18/landing_page_test/tests/TestBase.java
@@ -1,6 +1,7 @@
 package stream18.landing_page_test.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
@@ -14,18 +15,18 @@ import java.util.Map;
 public class TestBase {
 
 
-        @BeforeAll
-        static void beforeALL() {
-            Configuration.remote = "https://user1:1234@" + System.getProperty("1ofd_tests", "selenoid.autotests.cloud/wd/hub");
-            Configuration.browserSize = System.getProperty("browserSize");
-            Configuration.browser = System.getProperty("browser", "chrome");
-            Configuration.browserVersion = System.getProperty("browserVersion");
+    @BeforeAll
+    static void beforeALL() {
+        Configuration.remote = "https://user1:1234@" + System.getProperty("1ofd_tests", "selenoid.autotests.cloud/wd/hub");
+        Configuration.browserSize = System.getProperty("browserSize");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion");
 
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                    "enableVNC", true,
-                    "enableVideo", true
-            ));
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
 
             Configuration.browserCapabilities = capabilities;
         }
@@ -40,6 +41,7 @@ public class TestBase {
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
+        Selenide.closeWebDriver();
     }
 }
 
